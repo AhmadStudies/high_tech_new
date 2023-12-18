ea89a314a35954c8070cc77942137d81df314df8
Ahmad Shah Habib <ahmad.shahbcs@gmail.com>
2023-12-18 19:27:22 +0100

    added new code

--- src/main/java/controller/HighTechController.java
+++ src/main/java/controller/HighTechController.java
@@ -4,5 +4,11 @@ public class HighTechController {
 
     public static void main(String args[]){
         System.out.println("hello world");
+
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+
     }
 }
